06989b192304c3fd1bcaf83fa632b8d271fcff70
Neeraj Kashyap <neeraj@bugout.dev>
2020-10-23 20:37:50 -0700

    test_git_terminal: sample.py modifications

--- sample.py
+++ sample.py
@@ -1,2 +1,10 @@
+import argparse
+
 def hello(name: str):
     print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Hello world program")
+    parser.add_argument("-n", "--name", required=False, default="world", help="Name to greet")
+    args = parser.parse_args()
+    hello(args.name)
