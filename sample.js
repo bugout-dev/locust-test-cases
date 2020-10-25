2ebf72fe0e11f507f69419ccecc8060d4fa8b209
Neeraj Kashyap <nkashy1@gmail.com>
2020-10-24 19:15:50 -0700

    Changed where default is set

--- sample.js
+++ sample.js
@@ -1,6 +1,9 @@
 function hello(name) {
+  if (!name) {
+    name = "world";
+  }
   return `Hello, ${name}!`;
 }
 
-const name = process.argv[2] || "world";
+const name = process.argv[2];
 console.log(hello(name));
