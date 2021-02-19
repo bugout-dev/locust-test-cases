6722c1957cb72f489387e9bd58ef72c9833422ba
Neeraj Kashyap <nkashy1@gmail.com>
2021-02-19 13:37:09 -0800

    Added second method to Hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]);
+        System.out.println(hello() + args[0]);
+    }
+
+    private static String hello() {
+	    return "Hello ";
     }
 }
